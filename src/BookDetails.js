6e3bc470ca8c41e27ba840f62fd3e5f2b2749a76
poojakaturde <65528027+poojakaturde@users.noreply.github.com>
2021-10-07 11:41:40 +0530

    Add files via upload

---
+++ src/BookDetails.js
@@ -0,0 +1,136 @@
+const Biography = [
+    {
+        Book_Name = "Believe: What Life and Cricket Taught Me",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/41fpvivCkDL._SX323_BO1,204,203,200_.jpg",
+        Book_Description = "In this book, Suresh Raina takes us through the challenges he faced as a young cricketer. He was bullied in school and at cricket camps, but he always punched above his weight, overcoming every adversity life threw at him and never giving up. This is the story of the lessons he learnt and the friendships he built.",
+        Buy_link ="https://www.amazon.in/Believe-What-Life-Cricket-Taught/dp/0143454331"
+    },
+    {
+        Book_Name = "The Dhoni Touch ",
+        Book_Cover ="https://rukminim1.flixcart.com/image/832/832/kidgnm80-0/book/n/i/0/the-dhoni-touch-original-imafy6yyn7gynya4.jpeg?q=70",
+        Book_Description = "Be it his signature helicopter shot or his ever-calm demeanour, Mahendra Singh Dhoni has always found a way to tap into the hearts of his fans. For someone who was training to be a footballer, who could have told that Mahi was going to take the world of cricket by storm. In The Dhoni Touch: Unravelling the Enigma That Is Mahendra Singh Dhoni, Bharat Sundaresan reaches out to some of Mahi’s closest friends to find out how he became the man that he is today.",
+        Buy_link ="https://www.flipkart.com/the-dhoni-touch/p/itmf55c5ygy8fzvm"
+    },
+    {
+        Book_Name = "Wings of fire",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51vgy3LMz6L._SX339_BO1,204,203,200_.jpg",
+        Book_Description = "This is the story of Kalam's rise from obscurity and his personal and professional struggles, as well as the story of Agni, Prithvi, Akash, Trishul and Nag-missiles that have become household names in India and that have raised the nation to the level of a missile power of international reckoning. ",
+        Buy_link ="https://www.amazon.in/Wings-fire-P-Abdul-Kalam/dp/B0847V9BYD/ref=sr_1_2?dchild=1&hvadid=72980160943419&hvbmt=be&hvdev=c&hvqmt=e&keywords=wings+of+fire&qid=1633262984&qsid=259-2284536-4347056&sr=8-2&sres=8173711461%2CB0847V9BYD%2C9390590531%2C9390189179%2C935275087X%2C9352750853%2C9352750861%2C9352750926%2C9352752953%2C9352756916%2C9352750888%2CB07F6C99Q7%2C9352750918%2C9352752961%2C1338214489%2C9351864499&srpt=ABIS_BOOK"
+    },
+    {
+        Book_Name = "Unfinished: A Memoir",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/417FIfX3VTL._SX326_BO1,204,203,200_.jpg",
+        Book_Description = "Unfinished takes readers from Priyanka's childhood in India, where she was raised by her grandparents and her parents­-two army doctors committed not only to their children but to their careers and to philanthropy-before being sent away to boarding school at an early age; through her formative teenage years in the US living with extended family in the Midwest (Cedar Rapids and Indianapolis), Queens, and suburban Boston, where she endured bouts of racism; to her return to India, where she unexpectedly won the national and global beauty pageants (Miss India and Miss World) that launched her acting career.",
+        Buy_link ="https://www.amazon.in/Unfinished-Memoir-Priyanka-Chopra-Jonas/dp/0670094978"
+    },
+    {
+        Book_Name = "My Experiments With Truth",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/41Elw0UTCCL._SX322_BO1,204,203,200_.jpg",
+        Book_Description = "The Story of My Experiments with Truth’ is the compelling autobiography of Mohandas Karamchand Gandhi, who in his quest for freedom of his country, inspired millions with the strength of his willpower and personal actions. His immovable belief in truth and non-violence even in the most difficult situations made him a true hero. India has seen many heroes rise and sacrifice themselves for the greater good, but rarely has there been a man as great and as noble as Mahatma Gandhi who captured the imagination of the whole world. ",
+        Buy_link ="https://www.amazon.in/My-Experiments-Truth-Mohandas-Gandhi/dp/935064102X"
+    },
+];
+
+const ComicBook = [
+    {
+        Book_Name = "Avengers vs. Thanos",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/610wQFqMKPL._SX321_BO1,204,203,200_.jpg",
+        Book_Description = "his is a long compilation of various Marvel comics that all related to the Avengers vs Thanos storylines starting back in the mid-60s to the mid-70s and the first volume of the series leading to Infinity Wars.",
+        Buy_link ="https://www.amazon.in/Avengers-vs-Thanos-Jim-Starlin/dp/0785168508"
+    },
+    {
+        Book_Name = "Batman: Killing Joke",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51GgmyZXn4L._SX319_BO1,204,203,200_.jpg",
+        Book_Description = "THE KILLING JOKE is Alan Moore's fantastic reflection on the razor-thin line amongst rational sanity and craziness, boldness and villainy, drama and tragedy.As per the grinning engine of franticness and mayhem known as The Joker, that is all that isolates the normal from the psychotic. Freed once again from the confines of Arkham Asylum, he's out to demonstrate his deranged point. What's more he is going to utilize Gotham City's top cop, Commissioner Jim Gordon and his splendid and delightful girl Barbara to do it.",
+        Buy_link ="https://www.amazon.in/Batman-Killing-Deluxe-Alan-Moore/dp/1401216676/ref=sr_1_1_sspa?dchild=1&hvadid=73186319520362&hvbmt=bb&hvdev=c&hvqmt=b&keywords=batman+and+the+killing+joke&qid=1633263547&smid=A3H3WE9M6NY1KV&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFTRVBVTkRWUURVOEYmZW5jcnlwdGVkSWQ9QTA2MTA2MDk4MFJISjNWWDdKNTgmZW5jcnlwdGVkQWRJZD1BMDQ1OTMzOEowOUYwMUVEVDY1WiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+    },
+    {
+        Book_Name = "The Boy, The Mole, The Fox and The Horse",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/4138h5XntrL._SX389_BO1,204,203,200_.jpg",
+        Book_Description = "Enter the world of Charlie's four unlikely friends, discover their story and their most important life lessons.The boy, the mole, the fox and the horse have been shared millions of times online - perhaps you've seen them? They've also been recreated by children in schools and hung on hospital walls. They sometimes even appear on lamp posts and on cafe and bookshop windows.",
+        Buy_link ="https://www.amazon.in/Boy-Mole-Fox-Horse/dp/1529105102"
+    },
+    {
+        Book_Name = "The Complete MAUS",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/517h+3SR8aL._SX342_BO1,204,203,200_.jpg",
+        Book_Description = "Maus tells the story of Vladek Spiegelman, a Jewish survivor of Hitler's Europe, and his son, a cartoonist coming to terms with his father's story. Approaching the unspeakable through the diminutive (the Nazis are cats, the Jews mice), Vladek's harrowing story of survival is woven into the author's account of his tortured relationship with his aging father.",
+        Buy_link ="https://www.amazon.in/Complete-MAUS-Art-Spiegelman/dp/0141014083/ref=sr_1_1?dchild=1&hvadid=72499124549734&hvbmt=be&hvdev=c&hvqmt=e&keywords=complete+maus&qid=1633264004&qsid=259-2284536-4347056&sr=8-1&sres=0141014083%2CB09GYFWNSN%2CB00T3NMSOQ%2C3946458629%2CB09GYKPH62%2C0394747232%2C0679729771%2C0241455162%2C0393263126%2C607312581X%2C009952399X%2C8806202340%2C0241463971%2C0241463920%2C8174829164%2C8129141841&srpt=ABIS_BOOK"
+    },
+    {
+        Book_Name = "Akbar and Birbal: Famous Illustrated Tales",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/61Jy7Pj4TXL._SX342_BO1,204,203,200_.jpg",
+        Book_Description = "The tales of ‘Akbar and Birbal’ are one of the most famous folklores of India, popular amongst all age groups and enjoyed by everyone. These tales unfold the friendly and joyful association between king Akbar and his favourite courtier Birbal, known for his wit and wisdom.  Interesting narratives unfold about how the king tested time and again the shrewdness and clever insights of Birbal and how Birbal came up to the king’s expectations every time!",
+        Buy_link ="https://www.amazon.in/Akbar-Birbal-Illustrated-Maple-Press/dp/9350339072"
+    },
+];
+
+const Cooking = [
+    {
+        Book_Name = "Cravings: Hungry for More: A Cookbook",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51H2uCluCBL._SX400_BO1,204,203,200_.jpg",
+        Book_Description = "It’s a life of pancakes that remind you of blueberry pie, eating onion dip with your glam squad, banana bread that breaks the internet, and a little something called Pad Thai Carbonara. After two years of parenthood, falling in love with different flavors, and relearning the healing power of comfort food.",
+        Buy_link ="https://www.amazon.com/Cravings-Hungry-More-Chrissy-Teigen/dp/1524759724"
+    },
+    {
+        Book_Name = "Entice With Spice: Easy Indian Recipes",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51wnFjqrllL._SX451_BO1,204,203,200_.jpg",
+        Book_Description = "Entice with Spice shows Indian food enthusiasts how to prepare delicious meals at home without spending hours in the kitchen. A first-generation Indian-American, author Shubhra Ramineni has developed a no-fuss cooking style that re-creates authentic Indian flavors using easy techniques and fresh and readily available ingredients.",
+        Buy_link ="https://www.amazon.in/Entice-Spice-Indian-Recipes-Cookbook/dp/0804840296"
+    },
+    {
+        Book_Name = "Joy of Cooking",
+        Book_Cover ="https://m.media-amazon.com/images/P/1501169718.01._SCLZZZZZZZ_SX500_.jpg",
+        Book_Description = "Cooking shouldn’t just be about making a delicious dish—owning the process and enjoying the experience ought to be just as important as the meal itself. The new Joy of Cooking is a reminder that nothing can compare to gathering around the table for a home cooked meal with the people who matter most.",
+        Buy_link ="https://www.amazon.in/Joy-Cooking-Fully-Revised-Updated/dp/1501169718/ref=sr_1_2?dchild=1&hvadid=72292967172401&hvbmt=bp&hvdev=c&hvqmt=p&keywords=joy+of+cooking&qid=1633264400&qsid=259-2284536-4347056&sr=8-2&sres=1501169718%2C074321501X%2C0751335355%2C0451195159%2C0452256658%2CB09FP5HDDX%2CB098RGMD4C%2C0751335339%2C0743202090%2C074322518X%2C0743202104%2C0751335320%2C0743216806%2C0743206428%2C0451195140%2C0743216792&srpt=ABIS_BOOK"
+    },
+    {
+        Book_Name = "Violet Happy Baker",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51qzj8Qa0uL._SY498_BO1,204,203,200_.jpg",
+        Book_Description = "A passion for sweets doesn't have to be at odds with a healthy lifestyle.Thanks to the sugar-free or refined flour-free recipes that Violeta has been inventing and perfecting over the last few years, you can enjoy cupcakes, muffins, cakes, cookies, ice cream, chocolates and truffles in full bites and without regret.",
+        Buy_link ="https://www.amazon.in/Violeta-Happy-Baker-Violet/dp/8417752277/ref=sr_1_2?dchild=1&hvadid=72705283106036&hvbmt=be&hvdev=c&hvqmt=e&keywords=violet+bakery+cookbook&qid=1633264631&qsid=259-2284536-4347056&s=books&sr=1-2&sres=1607746719%2C8417752277%2C0857864416%2C1615190392%2C0224086790%2CB091FT8LV8%2CB091FTQVZ1%2CB091N67WQV%2CB091J6JNSL%2CB091J6C652%2CB091J5JZVQ%2CB091J4NXST%2CB091J4G2XB%2CB091J4965L%2CB091G17FJD%2CB091FV911R&srpt=ABIS_BOOK"
+    },
+    {
+        Book_Name = "Asian Dumplings: Mastering Gyoza, Spring Rolls, Samosas, and More",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51MhmHGzntL._SX353_BO1,204,203,200_.jpg",
+        Book_Description = "Asian Dumplings covers Eastern, Southeastern, and Southern Asia, with recipes from China, Japan, Korea, Nepal, Tibet, India, Thailand, Vietnam, Singapore, Malaysia, Indonesia, and the Philippines. Throughout, Nguyen shares the best techniques for shaping, filling, cooking, and serving each kind of dumpling.",
+        Buy_link ="https://www.amazon.in/Asian-Dumplings-Mastering-Spring-Samosas/dp/1580089755"
+    },
+];
+
+const Romance = [
+    {
+        Book_Name = "The Soulmate Equation",
+        Book_Cover ="https://m.media-amazon.com/images/P/1982123966.01._SCLZZZZZZZ_SX500_.jpg",
+        Book_Description = "This book has everything that makes romance novels great: a heroine's journey to self-discovery, a leading man worthy of a woman's love, and plenty of misty tears and full-on belly laughs along the way. Another knockout by Lauren.",
+        Buy_link ="https://www.amazon.in/Soulmate-Equation-Christina-Lauren/dp/1982123966"
+    },
+    {
+        Book_Name = "Accidentally Engaged",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/51Q5Vgl6-kL._SX326_BO1,204,203,200_.jpg",
+        Book_Description = "A delectable romantic comedy about a woman who fakes an engagement to the boy next door to enter a couples cooking contest.",
+        Buy_link ="https://www.amazon.com/Accidentally-Engaged-Farah-Heron/dp/1538734982"
+    },
+    {
+        Book_Name = "Wish I Could Tell You ",
+        Book_Cover ="https://images-eu.ssl-images-amazon.com/images/I/61XG5TJgxbL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+        Book_Description = "A disillusioned and heartbroken Anusha finds herself in the small world of WeDonate.com. Struggling to cope with her feelings and the job of raising money for charity, she reluctantly searches for a worthwhile cause to support.For Ananth, who has been on the opposite side, no life is less worthy, no cause too small to support.",
+        Buy_link ="https://www.amazon.in/Wish-I-Could-Tell-You/dp/0143448331"
+    },
+    {
+        Book_Name = "2 States: The Story Of My Marriage",
+        Book_Cover ="https://images-na.ssl-images-amazon.com/images/I/513UI-mvqYL._SX311_BO1,204,203,200_.jpg",
+        Book_Description = "2 States: The Story of My Marriage is autobiographical with only names changed. The story is about a couple Krish and Ananya, who hail from two different states of India, Punjab and Tamil Nadu, respectively, who are deeply in love and want to marry. ",
+        Buy_link ="https://www.amazon.in/2-States-Story-My-Marriage/dp/8129135523"
+    },
+    {
+        Book_Name = "It Ends with Us",
+        Book_Cover ="https://m.media-amazon.com/images/I/51Zu0ZwT0jL.jpg",
+        Book_Description = "It Ends with Us isn't an ordinary love story. It will break your heart while filling you with hope, and by the end of this gripping, pulse-pounding novel, you'll be smiling through your tears.",
+        Buy_link ="https://www.amazon.in/Ends-Us-Novel-Colleen-Hoover-ebook/dp/B0176M3U10"
+    },
+];
+
+export default Biography 
+export default Cooking 
+export default ComicBook 
+export default Romance 
